5f69198015db3ac2025fa4c7fcd1a5d47d200c6c
adbpush <97552635+adbpush@users.noreply.github.com>
2022-01-11 22:36:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,1 @@
 var url = 'https://discord.com';
-var start = (process.platform == 'darwin'? 'open': process.platform == 'win32'? 'start': 'xdg-open');
-require('child_process').exec(start + ' ' + url);
